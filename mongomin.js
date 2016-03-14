b14d6d6eb075cb72bd90dad7983b0181d1e73819
Saurin Dashadia <saurin@infyzo.in>
2016-03-14 21:01:14 +0530

    Chalk installed and used.

---
+++ mongomin.js
@@ -0,0 +1,10 @@
+/**
+ * @name mongomin
+ * @description Mongo DB admin by Symatic Solutions
+ * @license GPL-3.0
+ * @copyright Copyright(c) 2016 Symatic Solutions
+ */
+
+var chalk = require('chalk');
+
+console.log(chalk.green(" Mongomin Running"));
