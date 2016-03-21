131c09c315d24db7012f15eac4bb129a83ee5154
Saurin Dashadia <saurin@infyzo.in>
2016-03-21 20:52:11 +0530

    Async code added for file read write and user prompt if config.json file is missing.

--- mongomin.js
+++ mongomin.js
@@ -5,59 +5,62 @@
  * @copyright Copyright(c) 2016 Symatic Solutions
  */
 
-var MongoClient = require('mongodb').MongoClient;
 var chalk = require('chalk');
 var fs = require('fs');
 var prompt = require('prompt');
-var mongomin = require('./utility');
-
+var mongomin = require('./lib/core');
 
 console.log(chalk.green.bold("> Mongomin Started."));
 
-if(fs.existsSync('config.json')){
-    console.log(chalk.red("> Configuration file missing."));
-    console.log("> Getting connection parameters.\n");
+fs.exists('config.json', function(isExist){
+    if(isExist){
+        fs.readFile('config.json', function(err, data){
+            mongomin.config = data.toString();
 
-    prompt.message = chalk.green.bold("mongomin");
+            console.log("> Config data fetched.");
 
-    prompt.start();
+            mongomin.init();
+        });
+    }else{
+        console.log(chalk.red("> Configuration file missing."));
+        console.log("> Getting connection parameters.\n");
 
-    prompt.get({
-        properties: {
-            host: {
-                description: chalk.blue("mongoDB Host Name"),
-                default: '127.0.0.1'
-            },
-            port: {
-                description: chalk.blue("mongoDB Port"),
-                default: '27017'
-            },
-            username: {
-                description: chalk.blue("mongoDB Username"),
-                default: ''
-            },
-            password: {
-                description: chalk.blue("mongoDB Password"),
-                default: ''
+        prompt.message = chalk.green.bold("mongomin");
+
+        prompt.start();
+
+        prompt.get({
+            properties: {
+                host: {
+                    description: chalk.blue("mongoDB Host Name"),
+                    default: '127.0.0.1'
+                },
+                port: {
+                    description: chalk.blue("mongoDB Port"),
+                    default: '27017'
+                },
+                username: {
+                    description: chalk.blue("mongoDB Username"),
+                    default: ''
+                },
+                password: {
+                    description: chalk.blue("mongoDB Password"),
+                    default: ''
+                }
             }
-        }
-    }, function (err, data) {
-        console.log("\n> Creating config.json file.");
+        }, function (err, data) {
+            fs.writeFileSync('config.json', JSON.stringify(data));
 
-        fs.writeFileSync('config.json', JSON.stringify(data));
+            console.log("> Connection parameters written to config.json file.");
 
-        mongomin.config = JSON.parse(data.toString());
+            mongomin.config = data.toString();
 
-        console.log("> Connection parameters written to config.json file.");;
-    });
-}else{
-    console.log("> Fetching config data.");
-    var data = fs.readFileSync('config.json');
+            mongomin.init();
+        });
+    }
+});
 
-    mongomin.config = JSON.parse(data.toString());
-}
 
-mongomin.init();
 
 /*
 
