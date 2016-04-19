a71a6c1262bd6822756308e0e57799936bb1df70
devsaurin <dev@saur.in>
2016-04-19 18:50:10 +0530

    Basic controllers added.

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -6,64 +6,34 @@
  */
 
 angular.module('mongomin.controllers', [])
-    .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-        $scope.toggleLeft = buildDelayedToggler('left');
-        $scope.toggleRight = buildToggler('right');
-        $scope.isOpenRight = function(){
-            return $mdSidenav('right').isOpen();
-        };
-        /**
-         * Supplies a function that will continue to operate until the
-         * time is up.
-         */
-        function debounce(func, wait, context) {
-            var timer;
-            return function debounced() {
-                var context = $scope,
-                    args = Array.prototype.slice.call(arguments);
-                $timeout.cancel(timer);
-                timer = $timeout(function() {
-                    timer = undefined;
-                    func.apply(context, args);
-                }, wait || 10);
-            };
-        }
-        /**
-         * Build handler to open/close a SideNav; when animation finishes
-         * report completion in console
-         */
-        function buildDelayedToggler(navID) {
-            return debounce(function() {
-                $mdSidenav(navID)
-                    .toggle()
-                    .then(function () {
-                        $log.debug("toggle " + navID + " is done");
-                    });
-            }, 200);
-        }
-        function buildToggler(navID) {
-            return function() {
-                $mdSidenav(navID)
-                    .toggle()
-                    .then(function () {
-                        $log.debug("toggle " + navID + " is done");
-                    });
-            }
+    // Toolbar controller
+    .controller('ToolbarCtrl', function($scope, $mdSidenav, $mdMedia){
+        $scope.toggleSidebar = function(){
+            $mdSidenav('leftmenu')
+                .toggle()
+                .then(function () {
+                    console.log('done');
+                    //$log.debug("toggle " + navID + " is done");
+                });
         }
     })
-    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-        $scope.close = function () {
-            $mdSidenav('left').close()
-                .then(function () {
-                    $log.debug("close LEFT is done");
-                });
-        };
+
+    // Home controller
+    .controller('HomeCtrl', function($scope, $http){
+        console.log('Hello There');
     })
-    .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-        $scope.close = function () {
-            $mdSidenav('right').close()
-                .then(function () {
-                    $log.debug("close RIGHT is done");
-                });
-        };
+
+    //DBList controller
+    .controller('DBListCtrl', function($scope, $http){
+        $http
+            .get('/api/databases')
+            .then(function(res){
+                if(res.status === 200){
+                    if(res.data.ok === 1){
+                        $scope.databases = res.data.databases;
+                    }
+                }
+            }, function(res){
+
+            });
     });
